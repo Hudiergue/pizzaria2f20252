cbb3cd34ccf159925a7415223572425fad1c36ec
Hudiergue <rm95395@estudante.fieb.edu.br>
2025-09-26 08:26:49 -0300

    cadastro/produto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Produtos from "./pages/produtos"
-
+import CadastroProduto from "./pages/produtos/cadastro"
 const App = () => {
 
 
   return (
     <>
       <h1>Projeto Pizzaria</h1>
+      <CadastroProduto/>
       <Produtos />
     </>
   )
